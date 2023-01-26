08a5bd77f52da8ad06bce2ba12fb60f7a5445e6d
Andrii <andrewchepeliuk@gmail.com>
2023-01-26 13:43:07 +0200

    Drag & Move the window

--- SmalBusinessAdministrationWPF/View/MainView.xaml.cs
+++ SmalBusinessAdministrationWPF/View/MainView.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace SmallBusinessAdministrationWPF.View
 {
@@ -22,6 +25,21 @@ namespace SmallBusinessAdministrationWPF.View
         public MainView()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
     }
 }
