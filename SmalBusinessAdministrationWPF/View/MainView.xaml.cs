553796779d69e88dc3226260aa966412067d0f33
Andrii <andrewchepeliuk@gmail.com>
2023-01-26 15:58:43 +0200

    Added Control Bar Buttons

--- SmalBusinessAdministrationWPF/View/MainView.xaml.cs
+++ SmalBusinessAdministrationWPF/View/MainView.xaml.cs
@@ -41,5 +41,27 @@ namespace SmallBusinessAdministrationWPF.View
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
             this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
     }
 }
